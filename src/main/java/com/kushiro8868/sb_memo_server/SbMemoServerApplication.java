54d73aae562fc89a33b379e7f3e3cc3bc0f65039
Kushiro8868 <xorjs8868@gmail.com>
2025-03-05 17:07:48 +0900

    add

---
+++ src/main/java/com/kushiro8868/sb_memo_server/SbMemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.kushiro8868.sb_memo_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMemoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushiro8868/sb_memo_server/SbMemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushiro8868.sb_memo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMemoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
